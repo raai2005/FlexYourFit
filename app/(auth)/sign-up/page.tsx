fd30347835835279e0d9e4e4597631a53b43809b
Megha Roy <146658747+raai2005@users.noreply.github.com>
2026-01-12 23:38:43 +0530

    app-set-up

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignUpPage = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default SignUpPage;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import react from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage;
